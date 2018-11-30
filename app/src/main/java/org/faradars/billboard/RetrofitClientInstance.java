1ef77dece23eade57622d7add6f1d5c1dd59981e
mahsa <a.n_mahsa@rocketmail.com>
2018-11-30 22:41:06 +0330

    feat: Add retrofitclient

---
+++ app/src/main/java/org/faradars/billboard/RetrofitClientInstance.java
@@ -0,0 +1,19 @@
+package org.faradars.billboard;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://172.17.10.136:5000/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
