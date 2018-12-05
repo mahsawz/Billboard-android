372319b726c23e766fb340f48ae2065962d47014
farnazhs <hfarnaz1376@gmail.com>
2018-12-05 11:25:50 +0330

    fix login and authorization

--- app/src/main/java/org/faradars/billboard/RetrofitClientInstance.java
+++ app/src/main/java/org/faradars/billboard/RetrofitClientInstance.java
@@ -1,18 +1,38 @@
 package org.faradars.billboard;
 
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.JavaNetCookieJar;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
+
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://172.17.10.136:5000/";
+    private static final String BASE_URL = "http://0.0.0.0:5000/";
 
     public static Retrofit getRetrofitInstance() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        CookieHandler cookieHandler = new CookieManager();
+        OkHttpClient client = new OkHttpClient.Builder().addNetworkInterceptor(interceptor)
+                .cookieJar(new JavaNetCookieJar(cookieHandler))
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
         if (retrofit == null) {
-            retrofit = new retrofit2.Retrofit.Builder()
+            retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
+
         }
         return retrofit;
     }
