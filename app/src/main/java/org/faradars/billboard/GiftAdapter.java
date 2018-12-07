b596a1d86c55da0b024ef25553dbed3fd4cd1629
farnazhs <hfarnaz1376@gmail.com>
2018-12-07 21:59:56 +0330

    Feat:Add on item listener for gift redeem button

--- app/src/main/java/org/faradars/billboard/GiftAdapter.java
+++ app/src/main/java/org/faradars/billboard/GiftAdapter.java
@@ -5,7 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,24 +13,28 @@ import java.util.List;
 public class GiftAdapter extends RecyclerView.Adapter<GiftAdapter.MyViewHolder> {
 
     private List<Gift> giftList;
-    private AdapterView.OnItemClickListener onItemClickListener;
+
+    private GiftAdapter.OnItemClickListener onItemClickListener;
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView name, description, cost;
-        //public Button download;
+        public Button buy;
 
         public MyViewHolder(View view) {
             super(view);
             name = view.findViewById(R.id.name);
-            description =  view.findViewById(R.id.description);
+            description = view.findViewById(R.id.description);
             cost = view.findViewById(R.id.cost);
+            buy = view.findViewById(R.id.receive);
 
         }
     }
 
 
-    public GiftAdapter(List<Gift> giftList) {
+    public GiftAdapter(List<Gift> giftList,OnItemClickListener onItemClickListener) {
         this.giftList = giftList;
+        this.onItemClickListener = onItemClickListener;
     }
 
     @NonNull
@@ -43,17 +47,27 @@ public class GiftAdapter extends RecyclerView.Adapter<GiftAdapter.MyViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
         Gift gift = giftList.get(position);
         holder.name.setText(gift.getName());
-        holder.cost.setText(gift.getCost());
+        holder.cost.setText(String.valueOf(gift.getCost()));
         holder.description.setText(gift.getDescription());
-        //holder.download.setText(app.getDownload_link());
+        String giftCode = gift.getCode();
+        holder.buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onItemClickListener.onItemClicked(position);
+            }
+        });
         //holder.download.setOnClickListener(onItemClickListener.onItemClick());
     }
 
     @Override
     public int getItemCount() {
         return giftList.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClicked(int position);
     }
 }
--- app/src/main/java/org/faradars/billboard/ShowGiftActivity.java
+++ app/src/main/java/org/faradars/billboard/ShowGiftActivity.java
@@ -1,11 +1,13 @@
 package org.faradars.billboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +16,11 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ShowGiftActivity extends AppCompatActivity {
-    List<Gift> gifts=new ArrayList<>();
+public class ShowGiftActivity extends AppCompatActivity implements GiftAdapter.OnItemClickListener {
+    List<Gift> gifts = new ArrayList<>();
     private RecyclerView recyclerView;
     private GiftAdapter gAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,24 +30,46 @@ public class ShowGiftActivity extends AppCompatActivity {
         call.enqueue(new Callback<GiftShop>() {
             @Override
             public void onResponse(@NonNull Call<GiftShop> call, @NonNull Response<GiftShop> response) {
-                GiftShop giftRes=response.body();
-                gifts=giftRes.getGifts();
+                GiftShop giftRes = response.body();
+                assert giftRes != null;
+                gifts = giftRes.getGifts();
                 recyclerView = findViewById(R.id.giftRecycler);
-                gAdapter = new GiftAdapter(gifts);
+                gAdapter = new GiftAdapter(gifts, ShowGiftActivity.this);
                 RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
                 recyclerView.setLayoutManager(mLayoutManager);
                 recyclerView.setItemAnimator(new DefaultItemAnimator());
                 recyclerView.setAdapter(gAdapter);
-
             }
 
             @Override
             public void onFailure(@NonNull Call<GiftShop> call, @NonNull Throwable t) {
+                Toast.makeText(ShowGiftActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
+    @Override
+    public void onItemClicked(int position) {
+        Gift clicked = gifts.get(position);
+        int id = clicked.getId();
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<GiftShopResult> call = service.buyGift(id);
+        call.enqueue(new Callback<GiftShopResult>() {
+            @Override
+            public void onResponse(Call<GiftShopResult> call, Response<GiftShopResult> response) {
+                GiftShopResult shopRes = response.body();
+                assert shopRes != null;
+                if (shopRes.getStatus().equals("OK")) {
+                    ShoppingRecord records = shopRes.getRecord();
+                    Intent intent = new Intent(ShowGiftActivity.this, BuyGiftActivity.class);
+                    intent.putExtra("record", records);
+                    startActivity(intent);
+                }
+            }
+            @Override
+            public void onFailure(Call<GiftShopResult> call, Throwable t) {
 
             }
         });
-
-
     }
 }
