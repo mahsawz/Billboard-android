4bffd66512f4393922df4cbd5e1845c1d8476eb0
farnazhs <hfarnaz1376@gmail.com>
2018-12-21 22:09:50 +0330

    Fix: Add buy gift api to DataService class

---
+++ app/src/main/java/org/faradars/billboard/GetDataService.java
@@ -0,0 +1,30 @@
+package org.faradars.billboard;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface GetDataService {
+
+    @POST("/api/signup")
+    Call<UserResult> createUser(
+            @Body SignupInfo info);
+
+    @POST("/api/login")
+    Call<UserResult> loginUser(
+            @Body LoginInfo info);
+    
+    @GET("/api/giftshop")
+    Call<GiftShop> giftShop();
+
+    @GET("/api/showApps/")
+    Call<AppsResult> getapps();
+
+    @GET("/api/shoppingresult/{gift_id}")
+    Call<GiftShopResult> buyGift(@Path("gift_id") int id);
+
+   
+
+}
