fc932248de2aede6a026e2e694fae91e5eb85f02
mahsa <a.n_mahsa@rocketmail.com>
2018-12-22 09:11:58 +0330

    feat: Add getUser api

--- app/src/main/java/org/faradars/billboard/GetDataService.java
+++ app/src/main/java/org/faradars/billboard/GetDataService.java
@@ -1,20 +1,36 @@
-package org.billboard;
+package org.faradars.billboard;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
-public interface GetDataService <C>{
+public interface GetDataService<C> {
 
     @POST("/api/users")
     Call<User> createUser(
             @Body SignupInfo info);
 
     @POST("/api/login")
-    Call<User> loginUser(
+    Call<UserResult> loginUser(
             @Body LoginInfo info);
 
-    @POST("/api/showApps")
+    @GET("/api/showApps")
     Call<AppsResult> getapps();
+
+    @GET("/api/showGift")
+    Call<AppsResult> giftShop();
+
+    @GET("/api/buyGift")
+    Call<GiftShopResult> buyGift();
+
+    @GET("/api/gifthistory")
+    Call<GiftHistoryResult> getGiftHistory();
+
+    @GET("/api/shoppingresult/{gift_id}")
+    Call<GiftShopResult> buyGift(@Path("gift_id") int id);
+
+    @POST("/api/getUser")
+    Call<User> getUser(@Path("user_id") int id);
 }
