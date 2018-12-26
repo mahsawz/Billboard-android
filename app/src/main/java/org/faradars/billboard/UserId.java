c878a8ab0eb443d11f4c8338a9c3ea870e272ccd
farnazhs <hfarnaz1376@gmail.com>
2018-12-26 16:34:04 +0330

    Add userId model for send id for get user api

---
+++ app/src/main/java/org/faradars/billboard/UserId.java
@@ -0,0 +1,16 @@
+package org.faradars.billboard;
+
+public class UserId {
+    public int getUser_id() {
+        return user_id;
+    }
+
+    private int user_id;
+
+    public UserId(int user_id ) {
+
+        this.user_id = user_id;
+    }
+
+
+}
