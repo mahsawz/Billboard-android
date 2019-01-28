e7081affaa6c6ebb436ee9d2b6fabe1d78e4a800
mahsa <a.n_mahsa@rocketmail.com>
2019-01-28 17:28:42 +0330

    fix: edit survey class

--- app/src/main/java/org/faradars/billboard/Survey.java
+++ app/src/main/java/org/faradars/billboard/Survey.java
@@ -1,8 +1,13 @@
 package org.billboard;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class Survey {
+import java.util.List;
+
+public class Survey implements Parcelable {
 
     @SerializedName("description")
     private String description;
@@ -10,6 +15,14 @@ public class Survey {
     private String title;
     @SerializedName("id")
     private int id;
+    @SerializedName("questions")
+    private List<Question> questions;
+
+
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
 
     public String getTitle() {
         return title;
@@ -23,4 +36,34 @@ public class Survey {
         return id;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(description);
+        parcel.writeString(title);
+        parcel.writeInt(id);
+    }
+
+
+    protected Survey(Parcel in) {
+        description = in.readString();
+        title = in.readString();
+        id = in.readInt();
+    }
+
+    public static final Creator<Survey> CREATOR = new Creator<Survey>() {
+        @Override
+        public Survey createFromParcel(Parcel in) {
+            return new Survey(in);
+        }
+
+        @Override
+        public Survey[] newArray(int size) {
+            return new Survey[size];
+        }
+    };
 }
