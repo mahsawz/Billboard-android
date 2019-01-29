c74fb42bf36d29a6f8e7094a77985df0353d09d2
farnazhs <hfarnaz1376@gmail.com>
2019-01-29 19:52:45 +0330

    feat: Add filled survey model

---
+++ app/src/main/java/org/faradars/billboard/FilledSurveyInfo.java
@@ -0,0 +1,12 @@
+package org.faradars.billboard;
+
+import java.util.List;
+
+public class FilledSurveyInfo {
+    private List<String> items;
+
+    public FilledSurveyInfo(List<String> items) {
+        this.items=items;
+    }
+
+}
